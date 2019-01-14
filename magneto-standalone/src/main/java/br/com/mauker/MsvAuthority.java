3bd681d28663f37609d90e3569d174b3703e4409
jasontsang <jasontsang.icloud@me.com>
2019-01-14 10:49:50 +0800

    :sparkles: update travis.

--- magneto-standalone/src/main/java/br/com/mauker/MsvAuthority.java
+++
@@ -1,5 +0,0 @@
-package br.com.mauker;
-
-public class MsvAuthority {
-    public static final String CONTENT_AUTHORITY = "com.github.gnastnosaj.filter.magneto.standalone.searchhistorydatabase";
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,4 @@ rootProject.name = "filter"
 include("app")
 include("dsl")
 include("dsl-groovy")
-include("dsl-kotlin")
 include("magneto")
-include("magneto-standalone")
